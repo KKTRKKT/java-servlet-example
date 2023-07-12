cb2cf6993071cf0b9325dea83f498b98d747d67b
KKTRKKT <paulhana6006@gmail.com>
2023-07-12 17:56:16 +0900

    Servelt Listener 예제 추가

--- src/main/java/me/kktrkkt/servlet_demo/HelloServlet.java
+++ src/main/java/me/kktrkkt/servlet_demo/HelloServlet.java
@@ -7,9 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /*
-스프링 MVC의 기반 서블릿
-요청 당 쓰레드를 생성한다
-실행하기 위해서는 서블릿 엔진 또는 서블릿 컨테이너가 필요하다
+Servlet
+- 스프링 MVC의 기반이다
+- 요청 당 쓰레드를 생성한다
+- 실행하기 위해서는 서블릿 엔진 또는 서블릿 컨테이너가 필요하다
  */
 public class HelloServlet extends HttpServlet {
 
@@ -26,9 +27,13 @@ public class HelloServlet extends HttpServlet {
         resp.getWriter().println("<html>");
         resp.getWriter().println("<head></head>");
         resp.getWriter().println("<body>");
-        resp.getWriter().println("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("<h1>" + getName() + "</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</html>");
+    }
+
+    private Object getName() {
+        return getServletContext().getAttribute("name");
     }
 
     @Override
---
+++ src/main/java/me/kktrkkt/servlet_demo/MyListener.java
@@ -0,0 +1,26 @@
+package me.kktrkkt.servlet_demo;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/*
+Servlet Listener
+- 웹앱에서 발생하는 주요 이벤트을 감지하고, 추가 작업을 진행할 수 있다
+- 주요 이벤트로는 크게 서블릿 컨텍스트, 세션 이벤트가 있다.
+    - 각 주요 이벤트에는 라이프사이클, 애트리뷰트 변경 이벤트가 있다.
+ */
+public class MyListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // 서블릿 컨텍스트가 초기화될때 한번 실행된다
+        System.out.println("Context Initialized");
+        sce.getServletContext().setAttribute("name", "kktrkkt");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // 서블릿 컨텍스트 종료시점에 실행된다
+        System.out.println("Context Destroyed");
+    }
+}
