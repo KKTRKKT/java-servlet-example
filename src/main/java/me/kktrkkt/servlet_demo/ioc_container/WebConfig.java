f1f1630e453201f38992955e7fad6dcaf0a4120b
KKTRKKT <paulhana6006@gmail.com>
2023-07-17 17:45:57 +0900

    dispatcherServlet 설명 및 설정 예제 추가

---
+++ src/main/java/me/kktrkkt/servlet_demo/ioc_container/WebConfig.java
@@ -0,0 +1,11 @@
+package me.kktrkkt.servlet_demo.ioc_container;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+// DispathcerServlet의 ApplicationContext에 적용할 설정 파일
+@Configuration
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class)) // @Controller만 빈으로 등록한다
+public class WebConfig {
+}
