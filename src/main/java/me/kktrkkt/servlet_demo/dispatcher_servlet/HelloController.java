a5f3c096c44791a80d8f094d8249b985f83f8408
KKTRKKT <paulhana6006@gmail.com>
2023-07-19 22:24:53 +0900

    viewName 리턴 예제 추가

--- src/main/java/me/kktrkkt/servlet_demo/dispatcher_servlet/HelloController.java
+++ src/main/java/me/kktrkkt/servlet_demo/dispatcher_servlet/HelloController.java
@@ -1,17 +1,26 @@
 package me.kktrkkt.servlet_demo.dispatcher_servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     private HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello, " + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample(){
+        // viewResolver가 ModelAndView에 담아서 리턴해준다
+        return "/WEB-INF/templates/sample.jsp";
     }
 }
