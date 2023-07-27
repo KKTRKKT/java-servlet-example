38247af47913d8fb65942a1ef8ddab128f4e3ba2
KKTRKKT <paulhana6006@gmail.com>
2023-07-27 15:44:21 +0900

    WebMvcConfigure 예제 추가

--- src/main/java/me/kktrkkt/servlet_demo/enable_web_mvc/WebConfig.java
+++ src/main/java/me/kktrkkt/servlet_demo/enable_web_mvc/WebConfig.java
@@ -5,20 +5,28 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 // @EnableWebMvc를 DispatcherServlet을 구성하는 빈들에 영향을 미친다(order, 빈 추가 등)
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/templates/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/templates/");
+//        viewResolver.setSuffix(".jsp");
+//        return viewResolver;
+//    }
+
+
+    // @EnableWebMvc와 WebMvcConfigurer를 사용하면 설정 추가를 편하게 할 수 있다
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/templates/", ".jsp");
     }
-
 }
--- src/main/java/me/kktrkkt/servlet_demo/enable_web_mvc/package-info.java
+++ src/main/java/me/kktrkkt/servlet_demo/enable_web_mvc/package-info.java
@@ -3,4 +3,7 @@ package me.kktrkkt.servlet_demo.enable_web_mvc;
 /*
 @EnableWebMvc는 스프링 프레임워크에서 제공하는 MVC 설정 관련 애노테이션입니다
 DispatcherServlet에 사용할 빈을 일부 등록해줍니다. (Intercepter, Adapater, Converter 등)
+
+@EnableWebMvc를 설정하지 않으면 빈들을 일일히 생성하거나, 주입을 받아서 설정을 변경해줬지만,
+WebMvcConfigure와 같이 사용하면서 설정을 추가하고, 변경하는일이 더 편해졌다.
  */
